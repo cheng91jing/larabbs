e3db584b3559a24d1fa42e22f7d315cb37b91a3c
ChenJing <chen91jing@qq.com>
2018-04-14 14:16:58 +0000

    Finish user auth

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="navbar-header">
             <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapse" data-toggle="collapse" data-target="#app-navbar-collapse" >
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" >
                 <span class="sr-only">Toggle Navigation</span>{{--切换导航--}}
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -21,8 +21,33 @@
             <!-- Right Side Of Navbar 导航栏右侧-->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links 认证链接-->
-                <li><a href="#">登陆</a></li>
-                <li><a href="#">注册</a></li>
+                @guest
+                    <li><a href="{{ route('login') }}">登陆</a></li>
+                    <li><a href="{{ route('register') }}">注册</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
+                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                            </span>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    退出登录
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
