9c7c8395cae47472427c1f3e56e0e92c00102b18
ChenJing <chen91jing@qq.com>
2018-04-14 17:46:30 +0800

    Finish basic page layouts

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapse" data-toggle="collapse" data-target="#app-navbar-collapse" >
+                <span class="sr-only">Toggle Navigation</span>{{--切换导航--}}
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- Branding Image 品牌图片 -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                ChenBBS
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar 导航栏左侧-->
+            <ul class="nav navbar-nav">
+
+            </ul>
+            <!-- Right Side Of Navbar 导航栏右侧-->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links 认证链接-->
+                <li><a href="#">登陆</a></li>
+                <li><a href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
