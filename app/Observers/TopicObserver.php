708e160e0ac0ae0bf70612a98f8c1bc399f3027f
ChenJing <chen91jing@qq.com>
2018-04-19 21:45:53 +0800

    Users can edit their own topic

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,7 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        //由于 simditor js 插件会将html标签转义，所以，内容里面的标签不会被过滤
         $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
