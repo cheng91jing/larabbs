04f28b1f28665fce17455bfd44135e92f8f3c048
ChenJing <chen91jing@qq.com>
2018-04-21 14:27:13 +0800

    User can create reply

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+/**
+ * Class Reply
+ * @package App\Models
+ * @property Topic $topic
+ */
 class Reply extends Model
 {
     protected $fillable = ['content'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function topic()
     {
         return $this->belongsTo(Topic::class);
--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //topics 图片上传
 Route::post('upload_img', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
