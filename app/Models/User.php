fc6ea11eeab211cd841bebacdba9d4c1ad8dd553
ChenJing <chen91jing@qq.com>
2018-04-26 21:22:37 +0800

    User can register using phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar',
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
     ];
 
     /**
