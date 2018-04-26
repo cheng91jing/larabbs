6a17247dd6f76a74f7dabf49dbaf2256f526a7d4
ChenJing <chen91jing@qq.com>
2018-04-26 22:21:51 +0800

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\VerificationCodeRequest;
 use Illuminate\Http\Request;
+use Cache;
 
 class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request)
     {
-        $phone = $request->input('phone');
+        $captchaData = Cache::get($request->captcha_key);
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+
+        $phone = $captchaData['phone'];
         //生成随机数,左补0
         $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
@@ -31,7 +44,8 @@ class VerificationCodesController extends Controller
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
-        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
--- routes/api.php
+++ routes/api.php
@@ -27,12 +27,15 @@ $api->version('v1', [
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
-    ], function ($api){
+    ], function (Router $api){
         //短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
         //用户注册
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
+        //图片验证码
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
     });
 });
